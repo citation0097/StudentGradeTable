ef91c96e8295e361813734198f2000d9169911c6
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-08-30 16:33:53 -0700

    SGT v0.5

--- script.js
+++ script.js
@@ -41,9 +41,38 @@ function initializeApp(){
 *     
 */
 function addClickHandlersToElements(){
+
     $(".btn-success").click( handleAddClicked );
     $(".btn-default").click( handleCancelClick );
 
+    $(".btn-info").click( function(){
+
+        var data_object = { api_key: 'zgjo1f87H3' };
+        var ajaxParams = {
+            dataType: 'json',
+            url: 'https://s-apis.learningfuze.com/sgt/get',
+            method: 'post',
+            success: function( response ){
+                if(response.success){
+                    var student_ajax_array =[];
+                    // student_array = [];
+                    for( var i = 0 ;  i <  response.data.length; i++) {
+                        var    student = new Object();
+                        student.name = response.data[i].name;
+                        student.course =  response.data[i].course;
+                        student.grade =  response.data[i].grade;
+                        student_array.push(student);
+                        updateStudentList(student_array);
+                        clearAddStudentFormInputs();
+                    }
+                }else{
+                    return false;
+                };
+            },
+            data: data_object
+        }
+        $.ajax(ajaxParams);
+    });
 }
 
 /***************************************************************************************************
@@ -77,9 +106,9 @@ function addStudent(){
     student.name =  $('#studentName').val();
     student.course =  $('#course').val();
     student.grade =  $('#studentGrade').val();
-    console.log(student);
+    // console.log('addStudent',student);
+    if (student.name === ''){ return;}
     student_array.push(student);
-    console.log('student_array', student_array);
     updateStudentList(student_array);
     clearAddStudentFormInputs();
 }
@@ -87,8 +116,6 @@ function addStudent(){
  * clearAddStudentForm - clears out the form values based on inputIds variable
  */
 function clearAddStudentFormInputs(){
-    // student_array.pop();
-    // student = {};
     $('#studentName').val('');
     $('#course').val('');
     $('#studentGrade').val('') ;
@@ -98,17 +125,12 @@ function clearAddStudentFormInputs(){
  * into the .student_list tbody
  * @param {object} studentObj a single student object with course, name, and grade inside
  */
-function renderStudentOnDom(student_list){
-
-    //console.log('renderStudentOnDom',student_list[0].name);
+function renderStudentOnDom(studentObj){
 
     var row = $('<tr>');
-    row.attr('text' , currentRow);
-    row.attr('text' , "tr" + currentRow);
-    console.log('renderStudentOnDom',currentRow);
-    var column = '<td>' + student_list[currentRow].name + '</td>'
-        + '<td>' + student_list[currentRow].course + '</td>'
-        + '<td>' + student_list[currentRow].grade + '</td>'
+    var column = '<td>' + studentObj.name + '</td>'
+        + '<td>' + studentObj.course + '</td>'
+        + '<td>' + studentObj.grade + '</td>'
     var deleteBtn = $('<td>').attr('type','button').addClass('btn btn-danger');
     deleteBtn.text('Delete');
     var body = $(".table").find('tbody');
@@ -116,10 +138,15 @@ function renderStudentOnDom(student_list){
     row.append(deleteBtn);
     row.appendTo(body);
 
-    $(".btn-danger").click( function(){
+    deleteBtn.click( function(){
+        // console.log('btn studentObj',studentObj);
+        var stuIndex = student_array.indexOf(studentObj);
         var targetrow = $(event.currentTarget).parent();
         targetrow.remove();
-
+        // console.log('btn delete stuIndex',stuIndex);
+        student_array.splice(stuIndex,1);
+        var average = calculateGradeAverage(student_array);
+        renderGradeAverage(average);
     });
 
 }
@@ -131,27 +158,34 @@ function renderStudentOnDom(student_list){
  * @calls renderStudentOnDom, calculateGradeAverage, renderGradeAverage
  */
 function updateStudentList(student_list){
-
-    // for( var eachstudent in student_list ){
-        renderStudentOnDom(student_list);
-        // console.log('updateStudentList', student_list);
-        // console.log('updateStudentList_eachstudent', eachstudent);
-    // }
-    calculateGradeAverage();
+    $("tbody").empty();
+    for( var i = 0 ;  i < student_list.length ; i++ ) {
+        renderStudentOnDom(student_list[i]);
+    }
+    var average = calculateGradeAverage(student_list);
+    renderGradeAverage(average);
 }
 /***************************************************************************************************
  * calculateGradeAverage - loop through the global student array and calculate average grade and return that value
  * @param: {array} students  the array of student objects
  * @returns {number}
  */
-function calculateGradeAverage(){
+function calculateGradeAverage(student_list){
+    var sum = 0;
+
+    for( var i=0;  i < student_list.length ; i++ ){
+        sum += parseInt(student_list[i].grade);
+     //   console.log('student_list[i].grade',student_list[i].grade);
+    }
+    return parseInt(sum / (student_list.length));
 }
 /***************************************************************************************************
  * renderGradeAverage - updates the on-page grade average
  * @param: {number} average    the grade average
  * @returns {undefined} none
  */
-function renderGradeAverage(){
+function renderGradeAverage(average){
+    $(".badge").text( isNaN(average) ?  0 : average );
 }
 
 
