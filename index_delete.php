305a99a841d2a7ef7bf195cb3bbda657b09e6b93
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-10-30 17:37:56 -0700

    sgt edit 1030

--- index_delete.php
+++ index_delete.php
@@ -1,12 +1,17 @@
 <?php
     require_once("mysql_connect.php");
+
+    print_r($_POST);
+   
     $query = "DELETE FROM student_grade  WHERE student_id = '{$_POST['student_id']}'" ;          
+    print_r($query); 
     $result = mysqli_query($conn,$query);
+    print_r($result);
     $output =[
         'success'=> [],
         'error'  => []
     ];
-    
+    git
     if($result){
          $output['success'][] = true;
     }else{
